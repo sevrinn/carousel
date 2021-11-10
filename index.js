20f1fee9924c00389e7110f17cbcc89527433660
Sevrinn <sevrinnsevrinn@gmail.com>
2021-11-10 07:16:34 -0800

    grabbed carousel-items and put event listeners on buttons

---
+++ index.js
@@ -0,0 +1,14 @@
+const slides = document.querySelectorAll(".carousel-item")
+let slidePosition = 0
+const totalSlides = slides.length
+
+document.querySelector("#carousel-btn-prev").addEventListener("click", moveToNextSlide)
+document.querySelector("#carousel-btn-next").addEventListener("click", moveToPrevSlide)
+
+const moveToPrevSlide = () => {
+
+}
+
+const moveToNextSlide = () => {
+
+}
